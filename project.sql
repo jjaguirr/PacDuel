8373aad2533f6ccb840331d2689b5db3dae3592e
Aditya Mathur <69905281+adima1403@users.noreply.github.com>
2021-11-21 14:06:59 -0800

    Add files via upload
    sid and I have worked on the sql file so far and created 3 tables. - userinfo, friends and leaderboard. We have to figure out how to use JDBC to link the java code to the sql code.

---
+++ project.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS Project;
+CREATE DATABASE Project;
+
+USE Project;
+
+DROP TABLE IF EXISTS UserInfo;
+DROP TABLE IF EXISTS Friends;
+
+CREATE TABLE UserInfo (
+sno int NOT NULL,
+    username VARCHAR(15) NOT NULL,
+    email VARCHAR(25) UNIQUE,
+    passcode VARCHAR(12) NOT NULL,
+    PRIMARY KEY (sno),
+    FOREIGN KEY (username) REFERENCES Friends(username)
+);
+
+CREATE TABLE Friends(
+
+	sno int NOT NULL,
+    username VARCHAR(15) NOT NULL,
+    friend1 VARCHAR(15),
+    friend2 VARCHAR(15),
+    friend3 VARCHAR(15),
+    friend4 VARCHAR(15),
+    friend5 VARCHAR(15),
+    PRIMARY KEY(sno)
+);
+
+CREATE TABLE LeaderBoard(
+
+	sno int NOT NULL Primary key,
+    rankPlayer int NOT NULL, 
+    username VARCHAR(15) NOT NULL,
+	Score int NOT NULL 
+    );
+    
+    
+    
