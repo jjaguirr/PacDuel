cb8e1ea4c524edfb7ded1d60a96530afab27271b
David Schroeder <dfschroe@usc.edu>
2021-12-08 14:09:52 -0800

    chat

--- Pages/chat.php
+++ Pages/chat.php
@@ -1,29 +1,35 @@
 <?php
 require 'config.php';
-if(!$_POST["friend"]){
-    header('Location: home.php');
-}
-$friend=intval($_POST["friend"]);
 $host = "localhost";
-$port = 3456;
+$port = 3457;
 
 if ( ($socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === FALSE ) {
     echo "socket_create() failed: reason: " . socket_strerror(socket_last_error());
 }
 else {
-    echo "Attempting to connect to '$host' on port '$port'...<br>";
+    // echo "Attempting to connect to '$host' on port '$port'...<br>";
     if (($result = socket_connect($socket, $host, $port)) === FALSE) {
         echo "socket_connect() failed. Reason: ($result) " . socket_strerror(socket_last_error($socket));
     }
+}
+
+// if(!$_POST["friend"]){
+//     header('Location: home.php');
+// }
+$friend=intval($_POST["friend"]);
+
 
 $new_messages=array();
 
 if($_PUSH["outgoing_chat"] && !empty($_PUSH["outgoing_chat"])){
     array_push($new_messages,"u:".$_PUSH["outgoing_chat"]);
 }
+
 socket_write($socket,$_SESSION['sno']."\r\n", strlen ($_SESSION['sno']."\r\n"));
 socket_write($socket,$friend."\r\n", strlen ($friend."\r\n"));
-}
+$msg=socket_read($socket,2048)
+
+
 
 ?>
 <!DOCTYPE html>
@@ -39,6 +45,7 @@ socket_write($socket,$friend."\r\n", strlen ($friend."\r\n"));
 <body>
 	<?php include 'nav.php'; ?>
 <div id="messages">
+        <div class="incoming_message"><?php echo $msg;?></div>
 <?php foreach($_POST["messages"] as &$message):?>
     <?php if(substr($message,0,2)=="u:"):?>
         <div class="outgoing_message"><?php echo substr($message,2);?></div>
@@ -46,10 +53,8 @@ socket_write($socket,$friend."\r\n", strlen ($friend."\r\n"));
         <div class="incoming_message"><?php echo substr($message,2);?></div>
     <?php endif;?>
     <?php endforeach;?>
-    <?php while ($out = socket_read($socket, 2048)):?>
     <div class="incoming_message"><?php echo $out;?></div>
     <?php array_push($new_messages,"f:".$out)?>
-    <?php endwhile;?>
 </div>
 <form action="" method="POST" id="messagebox" onsubmit="addprefix()">
     <?php foreach($_POST["messages"] as &$message):?>
@@ -63,20 +68,17 @@ socket_write($socket,$friend."\r\n", strlen ($friend."\r\n"));
     <input type="text" class="form-control" placeholder="Text to chat..." name="outgoing_chat" id="outgoing_chat">
     <button type="submit" class="btn btn-primary" id="submitFormData">Send</button>
 </div>
-<button class="red btn" onclick="closechat()">End Chat</button>
+</form>
+<div class="endchat">
+<a href="home.php"><button class="red btn" >End Chat</button></a>
+</div>
 
 <script>
-function closechat(){
-    <?php 
-    $data="user disconnected";
-    socket_write ($socket, $data."\r\n", strlen ($data."\r\n"));
-    header('Location:home.php');
-    ?>
-}
 function addprefix(){
     var outgoing_chat=document.getElementById("outgoing_chat").value;
-    document.getElementById("outgoing_chat").setAttribute("value","u:"+outgoing_chat)
-
+    var newchat="u:"+outgoing_chat;
+    document.getElementById("outgoing_chat").setAttribute("value",newchat);
+    
 }
 </script>
 </body>
